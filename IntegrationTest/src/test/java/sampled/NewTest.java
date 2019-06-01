1bdf99c43660f908b4be8ddde6fea8960d2f0fdb
Balakrishna <balakrishna.godakhindi@gmail.com>
2019-06-01 18:07:39 +0530

    FirstPush

---
+++ IntegrationTest/src/test/java/sampled/NewTest.java
@@ -0,0 +1,24 @@
+package sampled;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void testGoogleTitle() {
+	  
+	  String path="C:\\Users\\Ganesh\\Desktop\\Balakrishna\\chromedriver_win32_1\\chromedriver.exe";
+	  
+	  System.setProperty("webdriver.chrome.driver", path);
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("http://www.google.co.in");
+	  String title = driver.getTitle();
+	  System.out.println(title);
+	  
+	  Assert.assertEquals(title, "Google");
+	  driver.close();
+  }
+}
+
